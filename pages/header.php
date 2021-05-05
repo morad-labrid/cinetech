8511d448dac079d32070b0ea626e89e5bbdd15e1
Nadir Ziane <nadir.ziane@laplateforme.io>
2021-05-05 15:05:44 +0200

    ajout du header, du head, et une partie de profil

---
+++ pages/header.php
@@ -0,0 +1,13 @@
+<header class="headerphp">
+    <div class="div1-header">
+        <h3>CINETECH</h3>
+        <nav>
+            <ul>
+                <li><a href="index.php ">Home</a></li>
+                <li><a href="index.php ">Films</a></li>
+                <li><a href="index.php ">Séries</a></li>
+                <li><a href="index.php ">Connexion</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
