36c7ec1eda8298b420af556e1e69949e7c2f00c9
Nadir Ziane <nadir.ziane@laplateforme.io>
2021-05-31 12:53:51 +0200

    ajout et envoie de commentaire

--- styles/js/scriptEnvoiecommentaire.js
+++ styles/js/scriptEnvoiecommentaire.js
@@ -1,26 +1,27 @@
-const params = new URLSearchParams(window.location.search);
-linkid = params.get('id');
-linktype = params.get('genre');
+$(document).ready(function() {
+    const params = new URLSearchParams(window.location.search);
+    linkid = params.get('id');
+    linktype = params.get('genre');
 
-$("#envoiecommentaire").click(function() {
-    console.log('btn ok');
-    var commentaire = $("#commentaire").val();
-    if (commentaire != '') {
-        $.ajax({
-            url: '../pages/element.php',
-            method: 'POST',
-            data: {
-                commentaire: commentaire
-            },
-            success: function(data) {
-                data = JSON.parse(data);
-                console.log(data);
-                if (data.login == user) {
-                    $(location).attr('href', 'profil.php');
-                } else($("#response").html(data))
-            }
-        })
-    } else {
-        $("#response").html('Remplissez le champs')
-    }
+    $("#envoiecommentaire").click(function() {
+        console.log('btn ok');
+        var commentaire = $("#commentaire").val();
+        if (commentaire != '') {
+            $.ajax({
+                url: '../pages/element.php',
+                method: 'POST',
+                data: {
+                    id: linkid,
+                    commentaire: commentaire
+                },
+                success: function(data) {
+                    console.log(ok);
+                    data = JSON.parse(data);
+                    $("#response").html(data)
+                }
+            })
+        } else {
+            $("#response").html('Remplissez le champs')
+        }
+    });
 })
